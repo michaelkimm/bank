c3a8295ef9581eda1585725d17365c478cb334c4
michael <mskim_451@dongguk.edu>
2023-03-16 16:43:38 +0900

    test : 입금 이체 내역 저장 x

--- src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleScheduler.java
+++ src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleScheduler.java
@@ -34,7 +34,7 @@ public class TransferReadOnceSendMultipleScheduler {
 
     @Transactional
 //    @Async("transferSchedulerAsyncExecutor")
-    @Scheduled(fixedDelay = 100)
+//    @Scheduled(fixedDelay = 100)
     public void processTransferOutBoxMessage() {
 
         List<ExternalTransferOutBox> outboxList = externalTransferOutBoxRepository.findAllExternalTransferOutBoxForUpdate();
@@ -61,7 +61,7 @@ public class TransferReadOnceSendMultipleScheduler {
     }
     @Transactional
 //    @Async("transferSchedulerAsyncExecutor")
-    @Scheduled(fixedDelay = 100)
+//    @Scheduled(fixedDelay = 100)
     public void processTransferDepositOutBoxMessage() {
 
         List<ExternalTransferDepositOutBox> outboxList = externalTransferDepositOutBoxRepository.findAllExternalTransferDepositOutBoxForUpdate();

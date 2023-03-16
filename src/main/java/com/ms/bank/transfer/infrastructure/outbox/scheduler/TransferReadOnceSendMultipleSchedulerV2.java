f646e4198cf3cab9186a48ea843cccf267eedf87
michael <mskim_451@dongguk.edu>
2023-03-16 10:46:43 +0900

    feature : change to TransferReadOnceSendMultipleSchedulerV2

--- src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
+++ src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
@@ -36,7 +36,7 @@ public class TransferReadOnceSendMultipleSchedulerV2 {
 
     @Transactional
 //    @Async("transferSchedulerAsyncExecutor")
-//    @Scheduled(fixedDelay = 100)
+    @Scheduled(fixedDelay = 100)
     public void processTransferOutBoxMessage() {
 
         List<ExternalTransferOutBox> outboxList = externalTransferOutBoxRepository.findAllExternalTransferOutBoxForUpdate();
@@ -61,7 +61,7 @@ public class TransferReadOnceSendMultipleSchedulerV2 {
 
     @Transactional
 //    @Async("transferSchedulerAsyncExecutor")
-//    @Scheduled(fixedDelay = 100)
+    @Scheduled(fixedDelay = 100)
     public void processTransferDepositOutBoxMessage() {
 
         List<ExternalTransferDepositOutBox> outboxList = externalTransferDepositOutBoxRepository.findAllExternalTransferDepositOutBoxForUpdate();

e6a4044b8cb211be63b42b1bb41fe29cee999c8c
michael <mskim_451@dongguk.edu>
2023-04-01 12:28:47 +0900

    test : dont do 출금

--- src/main/java/com/ms/bank/transfer/application/ExternalTransferService.java
+++ src/main/java/com/ms/bank/transfer/application/ExternalTransferService.java
@@ -42,18 +42,18 @@ public class ExternalTransferService {
     public void executeTransfer(final TransferRequestDto transferRequestDto) {
         // 이체 전 검증 진행
 
-        // 출금
-        Account withdrawalAccount = accountRepository.findByAccountNumberForUpdate(transferRequestDto.getWithdrawalAccountNumber())
-                .orElseThrow(() -> new RuntimeException("Account doesn't exist"));
-
-        if (!checkIfBalanceIsEnough(transferRequestDto, withdrawalAccount)) {
-            throw new RuntimeException("Withdrawal account money is not enough");
-        }
-        BigDecimal amountAfterWithdrawal = withdrawalAccount.getBalance().subtract(transferRequestDto.getTransferAmount());
-        withdrawalAccount.setBalance(amountAfterWithdrawal);
+//        // 출금
+//        Account withdrawalAccount = accountRepository.findByAccountNumberForUpdate(transferRequestDto.getWithdrawalAccountNumber())
+//                .orElseThrow(() -> new RuntimeException("Account doesn't exist"));
+//
+//        if (!checkIfBalanceIsEnough(transferRequestDto, withdrawalAccount)) {
+//            throw new RuntimeException("Withdrawal account money is not enough");
+//        }
+//        BigDecimal amountAfterWithdrawal = withdrawalAccount.getBalance().subtract(transferRequestDto.getTransferAmount());
+//        withdrawalAccount.setBalance(amountAfterWithdrawal);
 
         // 이체 내역(시작) 쌓기
-        TransferHistory transferHistory = toTransferHistory(transferRequestDto, amountAfterWithdrawal);
+        TransferHistory transferHistory = toTransferHistory(transferRequestDto, BigDecimal.ONE);
         transferHistoryRepository.save(transferHistory);
 
         // 이체 이벤트 쌓기

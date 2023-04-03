e3ebf1c3fa5b653540d9b6a7e71985e4d76716dd
michael <mskim_451@dongguk.edu>
2023-04-03 17:23:03 +0900

    feature : 은행 서버 ip 변경

--- src/main/java/com/ms/bank/transfer/infrastructure/Bank.java
+++ src/main/java/com/ms/bank/transfer/infrastructure/Bank.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 @Getter
 public enum Bank {
 
-    BANK1("01", "http://223.130.133.48:8080"),
-    BANK2("02", "http://175.45.202.115:8080"),
+    BANK1("01", "http://27.96.135.122:8080"),
+    BANK2("02", "http://49.50.161.86:8080"),
     NO_BANK("00", "http://localhost:8080");
 
     private String bankId;

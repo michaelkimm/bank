3a6fd913c0758a6576c71830f957c0136bbd00d8
michael <mskim_451@dongguk.edu>
2023-04-08 14:26:34 +0900

    test : 이체 입금 은행에서 이체 내역 저장 x

--- src/main/java/com/ms/bank/transfer/application/ExternalDepositService.java
+++ src/main/java/com/ms/bank/transfer/application/ExternalDepositService.java
@@ -57,7 +57,7 @@ public class ExternalDepositService {
 
         BigDecimal depositAmountResult = deposit(externalDepositRequestDto, account);
 
-        saveTransferHistory(externalDepositRequestDto, depositAmountResult);
+//        saveTransferHistory(externalDepositRequestDto, depositAmountResult);
     }
 
     public boolean executeSuccessProcess(ExternalDepositRequestDto externalDepositRequestDto) {

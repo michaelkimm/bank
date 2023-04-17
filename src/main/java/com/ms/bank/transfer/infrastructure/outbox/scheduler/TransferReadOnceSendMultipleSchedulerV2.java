ce2f206974504d96fc47061ea704b60b6f719f84
michael <mskim_451@dongguk.edu>
2023-04-17 11:11:01 +0900

    test : 스케줄러 off

--- src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
+++ src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
@@ -35,7 +35,7 @@ public class TransferReadOnceSendMultipleSchedulerV2 {
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
 //    @Transactional
-    @Scheduled(fixedDelay = 100)
+//    @Scheduled(fixedDelay = 100)
     public void processTransferOutBoxMessage() throws InterruptedException {
 
         List<ExternalTransferOutBox> outboxList = externalTransferOutBoxRepository.findAllExternalTransferOutBoxForUpdate();

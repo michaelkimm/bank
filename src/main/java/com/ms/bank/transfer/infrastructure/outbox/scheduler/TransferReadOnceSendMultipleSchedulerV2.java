d55429aed5aa8ffb30712757713b914ac3314289
michael <mskim_451@dongguk.edu>
2023-04-20 13:33:06 +0900

    test : repeatable read

--- src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
+++ src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
@@ -34,8 +34,8 @@ public class TransferReadOnceSendMultipleSchedulerV2 {
 
     private final ObjectMapper objectMapper;
 
-//    @Transactional
-    @Transactional(isolation = Isolation.READ_COMMITTED)
+//    @Transactional(isolation = Isolation.READ_COMMITTED)
+    @Transactional
     @Scheduled(fixedDelay = 100)
     public void processTransferOutBoxMessage() throws InterruptedException {
 

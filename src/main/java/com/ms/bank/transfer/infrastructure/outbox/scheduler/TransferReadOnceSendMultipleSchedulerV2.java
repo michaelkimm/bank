065a66b76db8d021f6cfe8781c4740ebeebe72b1
michael <mskim_451@dongguk.edu>
2023-04-20 14:41:33 +0900

    test : read committed

--- src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
+++ src/main/java/com/ms/bank/transfer/infrastructure/outbox/scheduler/TransferReadOnceSendMultipleSchedulerV2.java
@@ -34,8 +34,8 @@ public class TransferReadOnceSendMultipleSchedulerV2 {
 
     private final ObjectMapper objectMapper;
 
-//    @Transactional(isolation = Isolation.READ_COMMITTED)
-    @Transactional(isolation = Isolation.REPEATABLE_READ)
+//    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     @Scheduled(fixedDelay = 100)
     public void processTransferOutBoxMessage() throws InterruptedException {
 
